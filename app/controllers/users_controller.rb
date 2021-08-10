bd23c7e020c8e1b24312502de585115982fe88a8
Braj Pratap Singh <braj@Brajs-MacBook-Pro.local>
2021-08-10 18:34:54 +0530

    adding feature to archive user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,22 @@
 class UsersController < ApplicationController
   def index
-    render jsonapi: User.all
+		@users = User.filter(params)
+		render jsonapi: @users
+  end
+
+  def archive
+  	user = User.find_by(email: user_params[:email])
+  	if user
+  		response = user.mark_archive(current_user: current_user, archived: user_params[:archived])
+  		render json: response
+  	else
+			render json: { error: "Can't find User" }
+		end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:archived, :email)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,34 @@ class User < ApplicationRecord
   validates :email,
     presence: true,
     uniqueness: true
+
+  has_many :user_activities
+
+  after_save :notify_user
+
+  scope :archived_users, -> { where(archived: true) }
+  scope :unarchived_users, -> { where(archived: false) }
+
+  def mark_archive(current_user: current_user, archived: archived)
+  	response = if self == current_user
+  		{ error: "Can't archive yourself" }
+  	else
+  		update(archived: archived)
+  		activity = user_activities.create(updater_id: current_user.id, action: :archive)
+  		UserMailer.notify_user(activity.id)
+  		{ message: "Archived successfully"}
+  	end
+  	response
+  end
+
+  def self.filter(params)
+  	users = if params["archived"].nil?
+  		User.all
+  	elsif params["archived"].to_s == "true"
+  		User.archived_users
+  	else
+  		User.unarchived_users
+  	end
+  	users
+	end
 end
